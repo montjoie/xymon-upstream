c84476aab714eb279fc8c6afa5964e3c02017fb6
Corentin Labbe <clabbe.montjoie@gmail.com>
2026-01-23 11:41:50 +0100

    Add rockylinux10

--- docker/debian/start.sh
+++ docker/debian/start.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-pwd
-ls -l
-
 cd xymon
 
+# build with minimal deps
+./configure --server || exit $?
+make || exit $?
+
+apt-get install libc-ares-dev libldap-dev librrd-dev libssl-dev libtirpc-dev || exit $?
+
+# build with full deps
 ./configure --server || exit $?
 make || exit $?
